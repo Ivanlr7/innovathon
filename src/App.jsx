b23eb8f157c93c910bf88792828c9789544cc9c2
Iván Lorenzo <elhibar77@hotmail.es>
2025-10-24 09:57:40 +0200

    configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <div className='App'>
+
+    <main className='main-content'>
+    <Routes>
+
+    </Routes>
+    </main>
+    
+  </div>
+  )
+}
+
+export default App
